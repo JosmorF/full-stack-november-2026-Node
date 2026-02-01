a1dd1b6873c72bba195af51cf9dc4ebba4ade2a9
JosmorF <fabromo11@yahoo.es>
2026-02-01 09:18:39 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// import http from "node:http";
+
+// const PORT = 3001;
+
+// const server = http.createServer((req, res) => {
+//   console.log("puerto servido");
+//   res.end("se cierre conexion en puerto 3000....");
+// });
+
+// server.listen(PORT);
+
+console.log("trabaja");
